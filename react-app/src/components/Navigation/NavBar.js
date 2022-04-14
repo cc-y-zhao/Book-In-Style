b4f54e5eca4522b5f9910b91f0eb2d67508ece2a
cc-y-zhao <cecilia_zhao@berkeley.edu>
2022-04-14 16:08:13 -0700

    edited nav bar

--- react-app/src/components/Navigation/NavBar.js
+++ react-app/src/components/Navigation/NavBar.js
@@ -16,8 +16,9 @@ const NavBar = () => {
     sessionLinks = (
       <>
         <div className='logged-in-div'>
-          <ProfileButtonModal user={sessionUser} />
+          <NavLink to='/businesses/new' className='set-up-business'>Set Up My Business</NavLink>
           <span className='welcome-msg nav-element'>Welcome, {sessionUser.first_name}!</span>
+          <ProfileButtonModal user={sessionUser} />
         </div>
       </>
     );
