75ffe4667ed84b3cfc1810b81f1bd8c671c00d62
cc-y-zhao <cecilia_zhao@berkeley.edu>
2022-04-16 22:25:57 -0700

    styled 'see times' buttons

--- react-app/src/components/Modals/BookingModal.js
+++ react-app/src/components/Modals/BookingModal.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
 import CreateBookingForm from '../Forms/BookingForm';
 
+import '../Services/Services.css';
+
 function BookingModal({service, userId, businessId, businessName}) {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <div onClick={() => setShowModal(true)}>See Times</div>
+      <button className='see-times-btn' onClick={() => setShowModal(true)}>See Times</button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <CreateBookingForm setShowModal={setShowModal} service={service} userId={userId} businessId={businessId} businessName={businessName}/>
