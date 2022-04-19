333f3fcdf35b50e00990ba88ebae816308626970
cc-y-zhao <cecilia_zhao@berkeley.edu>
2022-04-19 10:02:29 -0700

    set up favorites store

---
+++ react-app/src/store/favorites.js
@@ -0,0 +1,52 @@
+const CREATE_FAVORITE = 'favorites/CREATE_FAVORITE';
+
+const createdFavorite = (favorite) => ({
+  type: CREATE_FAVORITE,
+  favorite
+});
+
+
+export const createFavorite = (favorite) => async (dispatch) => {
+
+  const response = await fetch('/api/favorites/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(favorite),
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    // console.log('data in action creator-----------', data)
+    dispatch(createdFavorite(data))
+    return data;
+  } else if (response.status < 500) {
+    const data = await response.json();
+    if (data.errors) {
+      return data.errors;
+    }
+  } else {
+    return ['An error occurred. Please try again.']
+  }
+}
+
+
+const initialState = {
+  'user': {},
+  'business': {},
+};
+
+export default function reducer(state = initialState, action) {
+  let newState = {...state};
+  switch (action.type) {
+    case CREATE_FAVORITE:
+      let newFavorite = action.favorite
+
+      return newState;
+
+
+    default:
+      return state;
+  }
+}
