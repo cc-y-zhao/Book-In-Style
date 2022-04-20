143640ef3e471746d2be2dac0becb07510e98c56
cc-y-zhao <cecilia_zhao@berkeley.edu>
2022-04-20 14:16:14 -0700

    added labels for all services on homepage

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -46,17 +46,44 @@ const HomePage = () => {
             <div className='homepage-img-1'></div>
             <div className='service-label'>WOMEN'S HAIRCUT</div>
           </div>
-          <div className='homepage-img-2'></div>
-          <div className='homepage-img-3'></div>
-          <div className='homepage-img-4'></div>
-          <div className='homepage-img-5'></div>
+          <div className='service-pic-label'>
+            <div className='homepage-img-2'></div>
+            <div className='service-label'>LASHES</div>
+          </div>
+          <div className='service-pic-label'>
+            <div className='homepage-img-3'></div>
+            <div className='service-label'>MEN'S HAIRCUT</div>
+          </div>
+          <div>
+            <div className='homepage-img-4'></div>
+            <div className='service-label'>SPA TREATMENT</div>
+          </div>
+          <div>
+            <div className='homepage-img-5'></div>
+            <div className='service-label'>NAILS</div>
+          </div>
         </div>
         <div className='services-homepage-top'>
-          <div className='homepage-img-6'></div>
-          <div className='homepage-img-7'></div>
-          <div className='homepage-img-8'></div>
-          <div className='homepage-img-9'></div>
-          <div className='homepage-img-10'></div>
+          <div>
+            <div className='homepage-img-6'></div>
+            <div className='service-label'>KID'S HAIRCUT</div>
+          </div>
+          <div>
+            <div className='homepage-img-7'></div>
+            <div className='service-label'>HAIR STYLING</div>
+          </div>
+          <div>
+            <div className='homepage-img-8'></div>
+            <div className='service-label'>MAKEUP</div>
+          </div>
+          <div>
+            <div className='homepage-img-9'></div>
+            <div className='service-label'>HAIR COLORING</div>
+          </div>
+          <div>
+            <div className='homepage-img-10'></div>
+            <div className='service-label'>PERM</div>
+          </div>
         </div>
       </div>
     </>
